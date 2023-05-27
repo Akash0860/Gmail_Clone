a5f2890594357582b125661001192aff7e24a053
Akash S <114648666+Akash0860@users.noreply.github.com>
2023-05-27 23:02:26 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const Connection = () =>{
    
     try {
-        mongoose.connect(DB_URI,{useNewUrlParser:true});
+        mongoose.connect(process.env.DB_URI,{useNewUrlParser:true});
         console.log('Database connected succesfully');
     } catch (error) {
         console.log('Error while connecting with the database',error.message)
